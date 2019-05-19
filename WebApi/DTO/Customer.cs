754c43fc7bc3e77ec01e12642d8f6a8d6492ac55
ShiraNarinsky <shiran79999@gmail.com>
2019-05-19 20:43:32 +0300

    ok

---
+++ WebApi/DTO/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    class Customer
+    {
+        public string nameCustomer { get; set; }
+        public string password { get; set; }
+        public int idCustomer { get; set; }
+        public string street { get; set; }
+        public string phone { get; set; }
+    }
+}
---
+++ WebApi/DTO/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    class Product
+    {
+        public int idProduct { get; set; }
+        public string nameProduct { get; set; }
+        public double price { get; set; }
+        public int unitsInStock { get; set; }
+        public int numCategory { get; set; }
+        public string color { get; set; }
+        public string model { get; set; }
+        public string size { get; set; }
+        public byte[] img { get; set; }
+    }
+}
