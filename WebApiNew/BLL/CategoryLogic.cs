e98f8ecb60af8125444c500aa9ad5fd396d59d60
EtiVaizman <e6739995@gmail.com>
2019-05-21 15:26:04 +0300

    חדש

---
+++ WebApiNew/BLL/CategoryLogic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+
+    public class CategoryLogic
+    {
+        public static DAL.SetTableEntities2 db = new DAL.SetTableEntities2();
+        public static List<DTO.Category> GetAllCategories()
+        {
+            List<DTO.Category> categoriesDto = new List<DTO.Category>();
+            List<DAL.Category> categories = db.Categories.ToList();
+            foreach (var item in categories)
+            {
+                categoriesDto.Add(DTO.Category.ConvertDBToDto(item));
+            }
+            return categoriesDto;
+        }
+    }
+}
---
+++ WebApiNew/Setable/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Setable.Controllers
+{
+    [RoutePrefix("api/category")]
+    public class CategoryController : ApiController
+    {
+        [Route("getAllCategories")]
+        public List<DTO.Category> GetAllCategories()
+        {
+            var x= BLL.CategoryLogic.GetAllCategories();
+            return x;
+        }
+    }
+}
